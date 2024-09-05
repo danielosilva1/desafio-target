1564a8bda0c2a616f79435039208275fd5c84696
dosilva <danielosilva@alu.ufc.br>
2024-09-04 21:50:21 -0300

    Habilita o export estático para fazer deploy no github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/desafio-target',
+    output: "export",  // <=== habilita o export estático
+    reactStrictMode: true,
+};
 
 export default nextConfig;
