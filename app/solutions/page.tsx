da16833dd0581ff4d12954e8dc28b135decca5c5
dosilva <danielosilva@alu.ufc.br>
2024-09-04 21:55:36 -0300

    Adiciona id às div's da página inicial para facilitar linkagem no readme

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -10,11 +10,21 @@ import styles from './page.module.css';
 export default function Solutions() {
     return (
         <div className={styles.mainContainer}>
-            <Sum></Sum>
-            <Fibonacci></Fibonacci>
-            <Revenue></Revenue>
-            <RevenuePerState></RevenuePerState>
-            <Reverse></Reverse>
+            <div id='q1'>
+                <Sum></Sum>
+            </div>
+            <div id='q2'>
+                <Fibonacci></Fibonacci>
+            </div>
+            <div id='q3'>
+                <Revenue></Revenue>
+            </div>
+            <div id='q4'>
+                <RevenuePerState></RevenuePerState>
+            </div>
+            <div id='q5'>
+                <Reverse></Reverse>
+            </div>
         </div>
     );
 }
